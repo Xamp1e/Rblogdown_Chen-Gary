60a6c806a0d252e386904a5ffaadd1f694c81a46
Xamp1e <m13072253523@163.com>
2023-05-04 09:58:18 +0800

    changing avatar

--- Zhangpeng_website_script.R
+++ Zhangpeng_website_script.R
@@ -1,13 +1,14 @@
-install.packages("blogdown")
+# when building new project on a new path, and can't find git file, then change the dir of git
+# git config --global http.sslCAInfo /path/to/ca-bundle.crt
+
+# install.packages("blogdown")
 library("blogdown")
-install_hugo()
-install_hugo(version = "0.83.1", force = TRUE)
-remove_hugo()
-hugo_version()
-new_site(theme = "tehillamo/academic-theme")
-
-new_site(theme = "Chen-Gary/hugo-theme-academic-old")
-
+# install_hugo()
+# install_hugo(version = "0.83.1", force = TRUE)
+# remove_hugo()
+# hugo_version()
+# new_site(theme = "tehillamo/academic-theme")
+# new_site(theme = "Chen-Gary/hugo-theme-academic-old")
 # new_site(theme = "wowchemy/starter-academic")
 
 stop_server()
